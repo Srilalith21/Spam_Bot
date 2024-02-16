34ab3d18e2f498e46b31d602fbaba1e76da7d250
Sri Lalith <111448265+Srilalith21@users.noreply.github.com>
2024-02-16 18:29:18 +0530

    Update Bot.java

--- Bot.java
+++ Bot.java
@@ -12,28 +12,22 @@ public class Bot{
 	public static void main(String[] args) throws InterruptedException, AWTException {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter Message :- ");
-		String msg = sc.nextLine();
+		String msg = sc.nextLine(); // Input String
 		System.out.println("Enter How Many Times to Send :- ");
-		int size = sc.nextInt();
-		
+		int size = sc.nextInt(); // Input Integer
 		StringSelection stringselection = new StringSelection(msg);
 		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 		clipboard.setContents(stringselection, null);
-		
-		Thread.sleep(4000);
-		
+		Thread.sleep(4000); // Delay sleep to open specific text editor
 		Robot robot = new Robot();
 		for(int i=1; i<=size; i++){
 			robot.keyPress(KeyEvent.VK_CONTROL);
 			robot.keyPress(KeyEvent.VK_V);
-			
 			robot.keyRelease(KeyEvent.VK_CONTROL);
 			robot.keyRelease(KeyEvent.VK_V);
-			
 			robot.keyPress(KeyEvent.VK_ENTER);
 			robot.keyRelease(KeyEvent.VK_ENTER);
-			
-			Thread.sleep(2000);
+			Thread.sleep(2000); // Sleep Time
 		}
 		System.out.println("Process completed...!");
 	}
